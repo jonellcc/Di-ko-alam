587a5ca5ffd4217757fb72d93523838359d8c756
Jonell Hutchin Magallanes <jonellmagallanescc102005@gmail.com>
2025-08-12 11:39:12 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import asyncio
 import os
 import time
 
-TARGET_URL = "http://www.noceco.ph"  # Replace with your target URL lol
+TARGET_URL = "https://lcccautomate.net/alams/signin.php"  # Replace with your target URL lol
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
